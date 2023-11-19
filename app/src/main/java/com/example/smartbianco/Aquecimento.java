62eedc6c1671b3f3d5ec5084d817db61d5d42639
Herick <h236663@dac.unicamp.br>
2023-11-18 23:04:50 -0300

    Verison 1

---
+++ app/src/main/java/com/example/smartbianco/Aquecimento.java
@@ -0,0 +1,173 @@
+package com.example.smartbianco;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class Aquecimento extends AppCompatActivity {
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aquecimento);}*/
+
+
+    /*private EditText aquecimentoInputTXT;
+    private TextView aquecimentoCronoTXT;
+    private Button  BtcAquecimentoSet;
+    private Button  BtcAquecimentoStartPause;
+    private Button  BtcAquecimentoReset;
+
+    private CountDownTimer  mCountDownTimer;
+    private boolean mTimerRunning;
+
+    private long    mIniciarTempoMillis;
+    private long    mTimeLeftMillis;
+    private long    mTempoFinal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aquecimento);
+
+        aquecimentoInputTXT = findViewById(R.id.aquecimentoInputXML);
+        aquecimentoCronoTXT = findViewById(R.id.aquecimentoCronoXML);
+
+        BtcAquecimentoSet = findViewById(R.id.aquecimentoSetXML);
+        BtcAquecimentoStartPause = findViewById(R.id.aquecimentoIniciarXML);
+        BtcAquecimentoReset = findViewById(R.id.aquecimentoResetXML);
+
+        BtcAquecimentoSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = aquecimentoInputTXT.getText().toString();
+                if (input.length() == 0 ){
+                    Toast.makeText(Aquecimento.this,"Não pode estar Vazio", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                long millisInput = Long.parseLong(input) * 6000;
+                if (millisInput == 0){
+                    Toast.makeText(Aquecimento.this,"Entre com Número Positivo", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                setTime(millisInput);
+                aquecimentoInputTXT.setText("");
+            }
+        });
+
+
+        BtcAquecimentoStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mTimerRunning){
+                    pauseTimer();
+                }else {
+                    startTime();
+                }
+            }
+        });
+
+        BtcAquecimentoReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+        });
+    } //
+
+
+    private void setTime(long milliseconds) {
+        mIniciarTempoMillis = milliseconds;
+        resetTimer();
+        closeKeyboard();
+    }
+
+
+    private void startTime() {
+        mTempoFinal = System.currentTimeMillis() + mTimeLeftMillis;
+
+        mCountDownTimer = new CountDownTimer(mTimeLeftMillis,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                //updateWatchInterface();
+            }
+        }.start();
+
+        mTimerRunning = true;
+        //updateWatchInterface();
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        //updateWatchInterface();
+    }
+
+    private void resetTimer() {
+        mTimeLeftMillis = mIniciarTempoMillis;
+        updateCountDownText();
+        //updateWatchInterface();
+    }
+
+
+
+    private void updateCountDownText() {
+        int horas       = (int) (mTimeLeftMillis / 1000) / 3600;
+        int minutos     = (int) ((mTimeLeftMillis / 1000) % 3600) / 60;
+        int segundos    = (int) (mTimeLeftMillis / 1000) % 60;
+
+        String timeLeftFormatted;
+        if (horas > 0) {
+            timeLeftFormatted = String.format(Locale.getDefault(), "%d:%02d:%02d",horas,minutos, segundos);
+        } else{
+            timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutos, segundos);
+        }
+
+        aquecimentoCronoTXT.setText(timeLeftFormatted);
+    }
+
+    *//*private void updateWatchInterface() {
+        if (mTimerRunning){
+        }
+    }*//*
+
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    *//*@Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences
+
+    }
+    *//*
+
+*/
+
+
+}// FINAL
---
+++ app/src/main/java/com/example/smartbianco/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.smartbianco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button aquecimentoBtc;
+    private Button skillBtc;
+    private Button wodBtc;
+    private Button alunosBtc;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        aquecimentoBtc = findViewById(R.id.aquecimentoXML);
+        skillBtc = findViewById(R.id.skillXML);
+        wodBtc = findViewById(R.id.wodXML);
+        alunosBtc = findViewById(R.id.alunosXML);
+
+        aquecimentoBtc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent aquecimentoIntent = new Intent(MainActivity.this, Aquecimento.class);
+                startActivity(aquecimentoIntent);
+            }
+        });
+
+        skillBtc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               /* Intent  skillIntent = new Intent(MainActivity.this, Skill.class);
+                startActivity(skillIntent);*/
+            }
+        });
+
+        wodBtc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Intent wodIntent = new Intent(MainActivity.this, Wod.class);
+                startActivity(wodIntent);*/
+            }
+        });
+
+        alunosBtc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               /* Intent alunosIntent = new Intent(MainActivity.this, Aula.class);
+                startActivity(alunosIntent);*/
+            }
+        });
+
+
+    }
+}
